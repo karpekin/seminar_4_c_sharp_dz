04690e9cb6b07bc8066294d38b546a268d2bf86b
karpekin <karpekin@mail.ru>
2023-04-19 00:56:14 +0300

    task 27

--- Program.cs
+++ Program.cs
@@ -3,21 +3,61 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write("Введите число А: ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
-int B = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число А: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
 
 
- int toPow(int num1, int num2)
- {
-    int result = 1;
-    for (int i = 1; i <= num2; i++ )
-    {
-        result *= num1;
-    }
-    return result;
- }
+//  int toPow(int num1, int num2)
+//  {
+//     int result = 1;
+//     for (int i = 1; i <= num2; i++ )
+//     {
+//         result *= num1;
+//     }
+//     return result;
+//  }
 
-int res = toPow(A,B);
-Console.WriteLine(res);
+// int res = toPow(A,B);
+// Console.WriteLine(res);
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Counter(int num)
+{
+    int i = 0;
+    while (num > 0)
+        {
+            num = num/10;
+            i++;
+        }
+    return i;
+}
+
+int count = Counter(number);
+
+int Summ(int num)
+{
+    int sum = 0;
+    for  (int j = 0; j < count; j++)
+        {
+    
+            sum = sum + num%10;
+            num = num/10;  
+    
+        }
+    return sum;
+}
+
+int s = Summ(number);
+Console.WriteLine(s);
+
+   
